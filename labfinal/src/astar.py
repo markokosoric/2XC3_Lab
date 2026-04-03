49057faaaad92338c38acb337bbcad81384e7910
patrick chen <chenp102+g@mcmaster.ca>
2026-04-03 14:04:14 -0400

    add A*

---
+++ labfinal/src/astar.py
@@ -0,0 +1,35 @@
+from typing import Dict
+from labfinal.src import min_heap
+from labfinal.src.final_project_part1 import DirectedWeightedGraph
+
+
+# From: https://en.wikipedia.org/wiki/A%2A_search_algorithm#Pseudocode
+def a_star(G: DirectedWeightedGraph, s: int, d: int, h: Dict[int, float]):
+    pred = {}
+    dist: Dict[int,float] = {} #Distance dictionary
+
+    Q = min_heap.MinHeap([])
+
+    nodes = list(G.adj.keys())
+
+    for node in nodes:
+        dist[node] = float("inf")
+        Q.insert(min_heap.Element(node, float("inf")))
+    dist[s] = 0;
+    Q.decrease_key(s, h[s])
+
+    while not Q.is_empty():
+        current_element = Q.extract_min()
+        current_node = current_element.value
+        if current_node == d: return pred
+
+        dist[current_node] = current_element.key
+        for neighbour in G.adj[current_node]:
+            score = dist[current_node] + float(G.w(current_node, neighbour));
+            if score < dist[neighbour]:
+                pred[neighbour] = current_node
+                dist[neighbour] = score;
+                Q.decrease_key(neighbour, score + h[neighbour]);
+
+    return pred
+
