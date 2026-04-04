df71dd80c4f456108be7bf43b665657725d65b0c
Marko Kosoric <116094253+markokosoric@users.noreply.github.com>
2026-04-03 22:45:33 -0400

    part 1

--- labfinal/src/final_project_part1.py
+++ labfinal/src/final_project_part1.py
@@ -114,3 +114,61 @@ def init_d(G):
                 d[i][j] = G.w(i, j)
         d[i][i] = 0
     return d
+
+
+def dijkstra_approx(G, source, k):
+    pred = {}
+    dist = {}
+    relax_count = {}
+    Q = min_heap.MinHeap([])
+    nodes = list(G.adj.keys())
+
+    for node in nodes:
+        Q.insert(min_heap.Element(node, float("inf")))
+        dist[node] = float("inf")
+        relax_count[node] = 0
+
+    dist[source] = 0
+    Q.decrease_key(source, 0)
+
+    while not Q.is_empty():
+        current_element = Q.extract_min()
+        current_node = current_element.value
+        dist[current_node] = current_element.key
+
+        for neighbour in G.adj[current_node]:
+            new_dist = dist[current_node] + G.w(current_node, neighbour)
+            if relax_count[neighbour] < k and new_dist < dist[neighbour]:
+                Q.decrease_key(neighbour, new_dist)
+                dist[neighbour] = new_dist
+                pred[neighbour] = current_node
+                relax_count[neighbour] += 1
+
+    return dist
+
+
+def bellman_ford_approx(G, source, k):
+    pred = {}
+    dist = {}
+    relax_count = {}
+    nodes = list(G.adj.keys())
+
+    for node in nodes:
+        dist[node] = float("inf")
+        relax_count[node] = 0
+    dist[source] = 0
+
+    for _ in range(G.number_of_nodes()):
+        updated = False
+        for node in nodes:
+            for neighbour in G.adj[node]:
+                new_dist = dist[node] + G.w(node, neighbour)
+                if relax_count[neighbour] < k and new_dist < dist[neighbour]:
+                    dist[neighbour] = new_dist
+                    pred[neighbour] = node
+                    relax_count[neighbour] += 1
+                    updated = True
+        if not updated:
+            break
+
+    return dist
